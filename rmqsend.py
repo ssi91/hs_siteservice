424b372c44d9c44a49c0de9aaab6c973518b9261
ssi <sergeev.s.i1991@gmail.com>
2015-11-01 16:49:50 +0600

    add port in ConnectionParameters

--- rmqsend.py
+++ rmqsend.py
@@ -3,7 +3,7 @@ import os
 
 
 def send(msg):
-	connection = pika.BlockingConnection(pika.ConnectionParameters(host = os.environ["HS_RMQ_PORT_25672_TCP_ADDR"]))
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host = os.environ["HS_RMQ_PORT_5672_TCP_ADDR"], port = 5672))
 	channel = connection.channel()
 	channel.queue_declare(queue = 'hello')
 	channel.basic_publish(exchange = '', routing_key = 'hello', body = msg)  # TODO проверить на валидность msg
